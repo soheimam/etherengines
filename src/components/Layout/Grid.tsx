c101a05f3448a8f0dc0bf4912ec512aa0bdbcbb9
Soheima Maradiaga <soheimam@gmail.com>
2023-07-07 19:40:29 +0200

    added grid

--- src/components/Layout/Grid.tsx
+++ src/components/Layout/Grid.tsx
@@ -1,7 +1,10 @@
 import React, { Children } from "react";
 
-function Grid({ Children }) {
-  return <section className="grid cols-12">{Children}</section>;
+interface IGrid {
+  children?: JSX.Element;
+}
+function Grid({ children }: IGrid) {
+  return <section className="grid cols-12">{children}</section>;
 }
 
 export default Grid;
