0abfaec7d096e3e12eadec4d936bf857cb625b12
Soheima Maradiaga <soheimam@gmail.com>
2023-07-08 20:35:18 +0200

    removed scroll

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,65 +1,39 @@
-import { useRef } from "react";
-import {
-  motion,
-  useScroll,
-  useSpring,
-  useTransform,
-  MotionValue,
-} from "framer-motion";
+import React, { useEffect, useRef } from "react";
 import Grid from "./Layout/Grid";
 
-function useParallax(value: MotionValue<number>, distance: number) {
-  return useTransform(value, [0, 1], [-distance, distance]);
-}
+function Homepage() {
+  const titleRef = useRef(null);
 
-function Image({ src, text }: { src: string; text: string }) {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({ target: ref });
-  const y = useParallax(scrollYProgress, 300);
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const { offsetX, offsetY, target } = e;
+      const { offsetWidth: width, offsetHeight: height } = target;
+      let moveX = (offsetX / width) * 100 - 50;
+      let moveY = (offsetY / height) * 100 - 50;
+
+      titleRef.current.style.backgroundPosition = `${moveX}% ${moveY}%`;
+    };
+
+    titleRef.current.addEventListener("mousemove", handleMouseMove);
+
+    // Clean up the event listener on unmount
+    return () => {
+      if (titleRef.current) {
+        titleRef.current.removeEventListener("mousemove", handleMouseMove);
+      }
+    };
+  }, []);
 
   return (
-    <section>
-      <div ref={ref}>
-        <img width={300} height={300} src={src} alt="A London skyscraper" />
-      </div>
-      <motion.h2 className="text-4xl " style={{ y }}>
-        {text}
-      </motion.h2>
-    </section>
+    <Grid>
+      <h1
+        ref={titleRef}
+        className="knockout bg-primary col-start-3 col-span-5 font-bold text-primary text-wrap"
+      >
+        PITSTOP PROTOCOL
+      </h1>
+    </Grid>
   );
 }
 
-const Home = () => {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
-
-  const imagesAndTexts = [
-    { src: "/images/scene.jpeg", text: "Text 1" },
-    { src: "/images/scene2.jpeg", text: "Text 2" },
-    { src: "/images/scene2.jpeg", text: "Text 3" },
-    { src: "/images/scene.jpeg", text: "Text 4" },
-    { src: "/images/scene.jpeg", text: "Text 5" },
-  ];
-
-  return (
-    <>
-      <Grid>
-        <h1 className="knockout bg-primary col-start-3 col-span-5 font-bold text-primary text-wrap">
-          PITSTOP PROTOCOL
-        </h1>
-      </Grid>
-      <section className="pt-60 flex flex-col justify-center align-center">
-        {imagesAndTexts.map((item, index) => (
-          <Image key={index} src={item.src} text={item.text} />
-        ))}
-        <motion.div className="progress bg-accent" style={{ scaleX }} />
-      </section>
-    </>
-  );
-};
-
-export default Home;
+export default Homepage;
