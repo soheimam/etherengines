953428c34576be6996b4f0af34a14553162b63ef
Soheima Maradiaga <soheimam@gmail.com>
2023-07-08 13:43:55 +0200

    selection fixed

--- src/components/LargeDriver.tsx
+++ src/components/LargeDriver.tsx
@@ -1,12 +1,27 @@
 import Image from "next/image";
 import React from "react";
+interface ILargeDriver {
+  driverImg: string;
+}
+import silhouteImg from "public/Images/silhoute.png";
 
-function LargeDriver() {
+function LargeDriver({ driverImg }: ILargeDriver) {
   return (
-    <article className="pose  col-span-5">
-      <div className="w-full h-96 bg-accent"></div>
+    <article className="pose  col-span-5 bg-neutral flex items-center flex-col bg-gradient-to-b from-gray-800 00 to-black">
+      <Image
+        alt={"non descript driver"}
+        src={
+          driverImg
+            ? `https://api.metafuse.me/assets/metafuse/${driverImg}.png`
+            : silhouteImg
+        }
+        priority={true}
+        width={300}
+        height={300}
+      />
+
       <div className="w-full py-4">
-        <h3 className="text-secondary"> cost</h3>
+        <h3 className="text-secondary font-bold"> {driverImg}</h3>
         <h3 className="text-secondary"> 0.05 ETH</h3>
       </div>
     </article>
--- src/components/MintView.tsx
+++ src/components/MintView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "./Layout/Grid";
 import LargeDriver from "./LargeDriver";
 import SmallDriver from "./SmallDriver";
@@ -41,18 +41,34 @@ const cars = [
 ];
 
 function MintView() {
+  const [selectedDrivers, setSelectedDrivers] = useState<string[]>([]);
+
+  const handleDriverSelect = (id: string) => {
+    if (selectedDrivers.includes(id)) {
+      setSelectedDrivers(selectedDrivers.filter((driver) => driver !== id));
+    } else if (selectedDrivers.length < 2) {
+      setSelectedDrivers([...selectedDrivers, id]);
+    }
+  };
+
   return (
     <Grid>
-      <LargeDriver />
+      {selectedDrivers.map((driver, index) => (
+        <LargeDriver driverImg={driver} key={index} />
+      ))}
       <div className="col-start-6 col-span-2 place-self-center">
-        <h3 className="text-primary"> Coins left</h3>
         <h3 className="text-primary"> Total Spent</h3>
         <button className="btn btn-primary">Mint</button>
       </div>
-      <LargeDriver />
       <section className="col-start-1 col-span-6 grid grid-cols-5 bg-neutral rounded-box">
         {drivers.map((driver) => (
-          <SmallDriver img={driver} key={driver} />
+          <SmallDriver
+            onSelect={handleDriverSelect}
+            img={driver}
+            key={driver}
+            selectedDrivers={selectedDrivers}
+            clickCount={selectedDrivers.length}
+          />
         ))}
       </section>
       <div className="col-start-7 col-span-full carousel  carousel-center  p-4 space-x-4 bg-neutral rounded-box">
