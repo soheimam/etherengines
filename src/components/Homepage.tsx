0ab4456131cef87bfa6a37550964e217f5174795
Soheima Maradiaga <soheimam@gmail.com>
2023-07-08 17:33:07 +0200

    added custom font

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,7 +4,7 @@ import Grid from "./Layout/Grid";
 function Homepage() {
   return (
     <Grid>
-      <h1 className="knockout bg-primary col-start-3 col-span-5 font-bold   text-primary text-wrap">
+      <h1 className="knockout bg-primary col-start-3 col-span-5 font-bold text-primary text-wrap">
         PITSTOP PROTOCOL
       </h1>
     </Grid>
