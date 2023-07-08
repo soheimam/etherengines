81a31438e51baa5ca0c0770d2c43e0d91bbf0f7b
Soheima Maradiaga <soheimam@gmail.com>
2023-07-08 16:05:29 +0200

    added custom font

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Grid from "./Layout/Grid";
+
+function Homepage() {
+  return (
+    <Grid>
+      <h1 className="text-6xl font-bold font-teko text-primary">
+        Pit stop protocol
+      </h1>
+    </Grid>
+  );
+}
+
+export default Homepage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        teko: ["Teko", "sans-serif"],
+      },
+    },
+  },
   daisyui: {
     themes: [
       {
