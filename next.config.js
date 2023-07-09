ce2255773b8df0978c883e655dbda7bd1548cad1
Quix <96739828+Quix44@users.noreply.github.com>
2023-07-09 10:31:06 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     ORACLE_ADDRESS: "0xB34cFc9dBfc555290B8345978992B6A4f7B14aF3",
     TOKEN_ADDRESS: "0xc21A904117530E34D44cca524496EfDf41022B22",
     METAFUSE_PROJECT_ID: "3ac14127-abd6-43ef-be99-c9fc635088cf",
-    METAFUSE_API_KEY: "5bcd940e-a02f-4b27-a054-6f8932bdb25e",
+    METAFUSE_API_KEY: "",
     METAFUSE_ITEMS_GATEWAY: "https://gateway.metafuse.me/v1/item",
   },
   webpack: (config) => {
